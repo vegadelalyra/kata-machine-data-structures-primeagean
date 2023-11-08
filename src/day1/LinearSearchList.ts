ccea5e17df0aa416298282685c106b3267362041
Vegadelalyra <lyradeperseo@gmail.com>
2023-11-08 11:09:07 -0500

    feat: implementing linear search list

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,6 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+    for (const hay of haystack) 
+        if (hay === needle) return true
+
+    return false
+}
